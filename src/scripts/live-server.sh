078535aeaeac286558ad151ace948b5aba7b5018
Suphakit P <me@suphakit.net>
2023-07-21 02:40:07 +0700

    feat<scripts>: live-server script!

---
+++ src/scripts/live-server.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "setting up `live-server` server config..."
+
+curl https://raw.githubusercontent.com/katzEco/katzuScript/main/source/live-server/server.sh --output live-server
+
+echo "Finished!"
